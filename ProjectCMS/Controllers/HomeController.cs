a8eb90b6e2acba87205a4eac62de4cc50cb6bebf
Manouchehrabdi <mabdi0258@gmail.com>
2024-06-09 12:38:16 +0330

    Slider 6

--- ProjectCMS/Controllers/HomeController.cs
+++ ProjectCMS/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MD.PersianDateTime.Standard;
+using ProjectShopCMS.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,17 @@ namespace ProjectShopCMS.Controllers
 {
     public class HomeController : Controller
     {
+        ProjectShopDBEntities db = new ProjectShopDBEntities();
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Slider()
+        {
+            DateTime dateTime = new DateTime(PersianDateTime.Now.Year, PersianDateTime.Now.Month, PersianDateTime.Now.Day, 0, 0, 0);
+            var getSlider = db.Slider.Where(s => s.IsActive && s.StartSliderDate >= dateTime && s.EndSliderDate>=dateTime);
+            return PartialView(getSlider);
         }
     }
 }
